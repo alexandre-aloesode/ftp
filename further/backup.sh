5fe630f5b72bb3742e6630010278ad1bc1b04453
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-10-07 13:39:34 +0200

    ajout further5

--- further/backup.sh
+++ further/backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mkdir /backups
-mkdir /backups/SystemConfigUsersFiles
-mkdir /backups/home
-cp /home/ /backups/home
-cp /etc/proftpd/proftpd.conf /backups/proftpd.conf
-cp /etc/proftpd/tls.conf /backups/tls.conf
-cp /etc/proftpd/modules.conf /backups/modules.conf
-tar zcvf /backups/SystemConfigUsersFiles/backup.tar.gz /backups/
+mkdir /backups/proftpdConfig-UsersFiles
+mkdir /backups/proftpdConfig-UsersFiles/proftpdconfig
+mkdir /backups/proftpdConfig-UsersFiles/UsersFiles
+mkdir /backups/archive
+cp /home -r /backups/proftpdConfig-UsersFiles/UsersFiles
+cp /etc/proftpd/proftpd.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/proftpd.conf
+cp /etc/proftpd/tls.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/tls.conf
+cp /etc/proftpd/modules.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/modules.conf
+tar zcvf /backups/archive/backup.tar.gz /backups/proftpdConfig-UsersFiles
