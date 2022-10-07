712c0072e200030311f3ae71398ad74750126f9b
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-10-07 13:05:28 +0200

    ajout further2

--- further/backup.sh
+++ further/backup.sh
@@ -3,7 +3,7 @@
 mkdir /backups/
 mkdir /backups/SystemConfigUsersFiles
 cp /home /backups/SystemConfigUsersFiles
-cp /etc/proftpd/proftpd.conf /backups/SystemConfigUsersFiles
-cp /etc/proftpd/tls.conf /backups/SystemConfigUsersFiles
-cp /etc/proftpd/modules.conf /backups/SystemConfigUsersFiles
-tar zcvf /backups/SystemConfigUsersFiles/backup.tar.gz /backups/SystemConfigUsersFiles
+cp /etc/proftpd/proftpd.conf /backups
+cp /etc/proftpd/tls.conf /backups
+cp /etc/proftpd/modules.conf /backups
+tar zcvf /backups/SystemConfigUsersFiles/backup.tar.gz /backups/
