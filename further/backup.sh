285a213f29e25893cc3ce128f7d413a3d7f9ba3f
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-10-07 13:50:06 +0200

    ajout further 1ere partie done

--- further/backup.sh
+++ further/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#######################1ERE Partie, création de l'archive#################################
 mkdir /backups
 mkdir /backups/proftpdConfig-UsersFiles
 mkdir /backups/proftpdConfig-UsersFiles/proftpdconfig
@@ -9,4 +10,6 @@ cp /home -r /backups/proftpdConfig-UsersFiles/UsersFiles
 cp /etc/proftpd/proftpd.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/proftpd.conf
 cp /etc/proftpd/tls.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/tls.conf
 cp /etc/proftpd/modules.conf /backups/proftpdConfig-UsersFiles/proftpdconfig/modules.conf
-tar zcvf /backups/archive/backup.tar.gz /backups/proftpdConfig-UsersFiles
+tar zcvf /backups/archive/backup_$(date +'%d-%m-%Y-%R').tar.gz /backups/proftpdConfig-UsersFiles
+
+#######################2EME Partie, envoi de l'archive####################################
